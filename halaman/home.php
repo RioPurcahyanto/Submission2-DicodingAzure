d854a98becde737ffc137f5ee202e0264139dcdb
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-05 22:05:50 +0700

    Update home.php

--- halaman/home.php
+++ halaman/home.php
@@ -4,10 +4,18 @@
 	<p>Please upload your file in button below</p>
 </div>
 
+<form action = "" method = "POST" enctype = "multipart/form-data">
+         <input type = "file" name = "image" />
+         <input type = "submit"/>
+			
+         <ul>
+            <li>Sent file: <?php echo $_FILES['image']['name'];  ?>
+            <li>File size: <?php echo $_FILES['image']['size'];  ?>
+            <li>File type: <?php echo $_FILES['image']['type'] ?>
+         </ul>
+			
+      </form>
+echo "<br />";
+echo "<br />";
 
-
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
 </form>
