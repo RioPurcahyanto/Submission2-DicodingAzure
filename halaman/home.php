5e47f109ab4a0935bbb303d9b26fedb60c0d666e
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-05 22:11:59 +0700

    Update home.php

--- halaman/home.php
+++ halaman/home.php
@@ -2,20 +2,22 @@
 	<h2>Halaman Depan</h2>
 	<p>Welcome to this site</p>
 	<p>Please upload your file in button below</p>
+	
+	<?php
+    if (isset($_SESSION['message']) && $_SESSION['message'])
+    {
+      printf('<b>%s</b>', $_SESSION['message']);
+      unset($_SESSION['message']);
+    }
+  ?>
+  <form method="POST" action="upload.php" enctype="multipart/form-data">
+    <div>
+      <span>Upload a File:</span>
+      <input type="file" name="uploadedFile" />
+    </div>
+ 
+    <input type="submit" name="uploadBtn" value="Upload" />
+  </form>
+
 </div>
 
-<form action = "" method = "POST" enctype = "multipart/form-data">
-         <input type = "file" name = "image" />
-         <input type = "submit"/>
-			
-         <ul>
-            <li>Sent file: <?php echo $_FILES['image']['name'];  ?>
-            <li>File size: <?php echo $_FILES['image']['size'];  ?>
-            <li>File type: <?php echo $_FILES['image']['type'] ?>
-         </ul>
-			
-      </form>
-echo "<br />";
-echo "<br />";
-
-</form>
