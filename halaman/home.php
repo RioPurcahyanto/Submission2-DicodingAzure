8b26578fdbe8d24c9ec756c8f3c4854e9ad15b97
Rio Purcahyanto <riopurcahyantp.x3@gmail.com>
2019-04-05 20:23:18 +0700

    Initial commit

---
+++ halaman/home.php
@@ -0,0 +1,13 @@
+<div class="halaman">
+	<h2>Halaman Depan</h2>
+	<p>Welcome to this site</p>
+	<p>Please upload your file in button below</p>
+</div>
+
+
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
---
+++ halaman/tentang.php
@@ -0,0 +1,5 @@
+<div class="halaman">
+	<h2>Tentang Kami</h2>
+	
+	<p>This is supporting website for Dicoding - Azure - Rio's Submission2</p>
+</div>
