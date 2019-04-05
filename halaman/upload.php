d5997684468528ec85e0dd9100290dbbff16b16e
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-05 23:41:44 +0700

    Update upload.php

--- halaman/upload.php
+++ halaman/upload.php
@@ -1,6 +1,7 @@
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+echo $target_file
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
