a7e6698a45d5053aabf3249c92a84d0d6d5ca56c
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-05 23:12:24 +0700

    Update phpQS.php

--- halaman/phpQS.php
+++ halaman/phpQS.php
@@ -32,6 +32,52 @@
 #
 **/
 
+//handle image
+$target_dir = "halaman/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+//end of handle
+
 require_once 'vendor/autoload.php';
 require_once "./halaman/random_string.php";
 
@@ -46,12 +92,9 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=bmpnjstoragetest
 
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
-$dir = "/halaman/uploads";
-$fileToUpload = opendir($dir)
-while (false !== ($filename = readdir($dh))) {
-    $files[] = $filename;
-}
-$images=preg_grep ('/\.jpg$/i', $files);
+
+$fileToUpload = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
 
 if (!isset($_GET["Cleanup"])) {
     // Create container options object.
