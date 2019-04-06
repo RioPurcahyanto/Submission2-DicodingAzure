f341d1b42e7618ac1ed73b2c8a119d7157efcf68
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-06 09:45:12 +0700

    Update upload.php

--- halaman/upload.php
+++ halaman/upload.php
@@ -37,7 +37,8 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        
+        header("Location: bmpnj-test.azurewebsites.net/index.php"); /* Redirect browser */
+exit();
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
