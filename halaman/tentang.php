07b73e83b4845a69681910b6b7cfa6f37f0bece6
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-06 22:07:45 +0700

    Update tentang.php

--- halaman/tentang.php
+++ halaman/tentang.php
@@ -2,5 +2,7 @@
 	<h2>Tentang Kami</h2>
 	
 	<p>This is supporting website for Dicoding - Azure - Rio's Submission2</p>
-
+	
+	$files = glob("D:\home\site\wwwroot\halaman\uploads\*.{jpg,gif,png}", GLOB_BRACE);
+	print_r($files)
 </div>
