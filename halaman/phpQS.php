c5b57c440243291d84b5818920866dac8256ade0
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-07 01:25:26 +0700

    Update phpQS.php

--- halaman/phpQS.php
+++ halaman/phpQS.php
@@ -167,7 +167,7 @@ else
     }
 }
 ?>
-<img src=$filePath>
+<img src=<?php echo $filePath; ?> >
 
 <form method="post" action="/halaman/phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
     <button type="submit">Press to clean up all resources created by this sample</button>
