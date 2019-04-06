ed6e2b2f687ca24c661f4004de975e999c9a5eee
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-06 10:20:16 +0700

    Update tentang.php

--- halaman/tentang.php
+++ halaman/tentang.php
@@ -1,14 +1,20 @@
-<?php
 <div class="halaman">
 	<h2>Tentang Kami</h2>
 	
 	<p>This is supporting website for Dicoding - Azure - Rio's Submission2</p>
-	
-$dir = "/halaman/uploads/";
+
+</div>
+
+<?php
+$dir = "/images/";
 
 // Sort in ascending order - this is default
 $a = scandir($dir);
 
+// Sort in descending order
+$b = scandir($dir,1);
+
 print_r($a);
-</div>
+print_r($b);
+?>
 ?>
