01afe32b8549ec0e0d0dc553c3f46239a23fb2bc
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-07 01:59:51 +0700

    Update phpQS.php

--- halaman/phpQS.php
+++ halaman/phpQS.php
@@ -127,7 +127,7 @@ if (!isset($_GET["Cleanup"])) {
 	$blob = $blobClient->getBlob($containerName, $fileToUpload);
 //         fpassthru($blob->getContentStream());
 	$imgUrl = $blob->getUrl();
-	
+	echo <img src=".$imgUrl.">
         echo "<br />";
     }
     catch(ServiceException $e){
@@ -167,7 +167,7 @@ else
     }
 }
 ?>
-<img src="<?php echo $imgUrl?>">
+<!-- <img src="<?php echo $imgUrl?>"> -->
 
 <!-- <form method="post" action="/halaman/phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
     <button type="submit">Press to clean up all resources created by this sample</button>
