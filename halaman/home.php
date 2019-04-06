5421d3082eb68da90babd1324f3630ca87bd9742
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-06 22:56:03 +0700

    Update home.php

--- halaman/home.php
+++ halaman/home.php
@@ -2,7 +2,7 @@
 	<h2>Welcome to this site</h2>
 	<p>Please upload your file in button below</p>
 	<br><br><br>
-	<form action="/halaman/upload.php" method="post" enctype="multipart/form-data">
+	<form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
