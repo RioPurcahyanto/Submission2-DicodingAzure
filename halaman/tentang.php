efd6d987a995c0e03b98a1e3923bb05a340f2ba7
RioPurcahyanto <49023823+RioPurcahyanto@users.noreply.github.com>
2019-04-07 03:23:28 +0700

    Update tentang.php

--- halaman/tentang.php
+++ halaman/tentang.php
@@ -8,10 +8,6 @@
 <?php
  	$files = glob("D:\home\site\wwwroot\halaman\*.{jpg,gif,png}", GLOB_BRACE);
 	usort( $files, function( $a, $b ) { return filemtime($b) - filemtime($a); } );
-// 	print_r($files);
-	$fileToUpload = $files[0];
-	$filePath = $files;
-	$fileToUpload = basename($filePath);
-// 	print("this is file path".$filePath."\n");
-	print("this is fileToUpload".$fileToUpload ."<br />");
+	print_r($files);
+	
 ?>
